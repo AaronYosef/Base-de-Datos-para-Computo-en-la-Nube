74f2b3100f1204687c6babdcad889a9ad43ea6cf
AaronYosef <22300098@uttt.edu.mx>
2024-03-20 02:40:58 -0600

    Scripts completos

---
+++ Practica-1-Consultas/practica.sh
@@ -0,0 +1,18 @@
+use practica1;
+db.createCollection("facturas");
+show dbs;
+db.facturas.find();
+db.productos.insertOne();
+db.productos.deleteOne();
+show collections;
+db.empleados.find();
+db.empleados.find({"empresa":{$eq:"Google"}});
+db.empleados.find({"pais": {$eq: "Peru"}});
+db.empleados.find({"salario": {$gt:8000}});
+b.empleados.find({"ventas": {$lt:10000}});
+db.empleados.findOne({"ventas": {$lt:10000}});
+db.empleados.find({$or:[{"empresa": "Google"}, {"empresa": "Yahoo"}]});
+db.empleados.find({“empresa": {$in:["Google",”Yahoo"]}});
+db.empleados.find({$and:[{"empresa":{$eq:"Amazon"}},{"salario":{$gt:9000}}]});
+db.empleados.find({$or: [{$and: [{empresa: "Yahoo"}, {salario: {$gt:6000}}]},{$and:[{empresa: "Google"}, {ventas: {$lt:20000}}]}]};
+db.empleados.find({},{_id:0,nombre:1, apellidos:1,pais:1});
